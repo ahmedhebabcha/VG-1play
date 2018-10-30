40295902bcec33b43307062ccccf62e4feeaf343
ahmedhebabcha <44622659+ahmedhebabcha@users.noreply.github.com>
2018-10-30 23:15:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("خرا",{type: 'WATCHING'})
+   client.user.setActivity("test",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
